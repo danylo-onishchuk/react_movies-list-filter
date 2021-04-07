b81378c0db4ff8998efdf4da8f5b68c87fc3f920
Onishchuk <onishchuk.danil@gmail.com>
2021-04-07 13:04:33 +0300

    Updates

---
+++ precache-manifest.8c8518ca208d624dbbf3c3e04abe2ecc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b95ec3bad9bf81b5cd2a917f26b9b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a764c90c198ca22c4e9a",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "316d4e94e17f2bc09987",
+    "url": "./static/css/main.9f604ba9.chunk.css"
+  },
+  {
+    "revision": "a764c90c198ca22c4e9a",
+    "url": "./static/js/2.ff0d155a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ff0d155a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316d4e94e17f2bc09987",
+    "url": "./static/js/main.3f0abb8f.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
